ad88e907a558ac7b9956d163ad05c2cb8cf59987
BabyDell <A.Java.Coder7@gmail.com>
2024-06-29 15:42:22 -0700

    updated images, and made layout responsive

--- src/app/Componenets/Projects.tsx
+++ src/app/Componenets/Projects.tsx
@@ -4,12 +4,20 @@ export default function Projects() {
       <span className="flex justify-center text-5xl font-serif pt-8 mb-16">
         Projects
       </span>
-      <div className=" flex grid-cols-2 justify-center">
-        <img src="/NotionProject.png" className="w-1/4 mr-16" />
-        <div className="w-1/4 ml-16 text-xl font-serif">
-          An application built using React, MongoDb, TypeScript, and Tailwind
-          with another programmer. It is a doumentation page where you can keep
-          files such as to do list, calenders, and files. 
+      <div className="w-full flex justify-center items-center">
+        <div className=" flex grid grid-cols-2 gap-20 w-full mr-5 ml-5 lg:w-3/4">
+          <img src="/NotionProject.png" className="" />
+          <div className="w-auto  text-2xl font-serif">
+            An application built using React, MongoDb, TypeScript, and Tailwind
+            with another programmer. It is a doumentation page where you can
+            keep files such as to do list, calenders, and files. In Progress.
+          </div>
+          <img src="Fashionista-Website.png" />
+          <div className="w-auto  text-2xl font-serif">
+            A Website focusing on fashion posts. People can find inpsirations
+            for new outfits, accessories, or shoes. Built using NextJS,
+            Tailwind, and React. Still in progress.
+          </div>
         </div>
       </div>
     </div>
